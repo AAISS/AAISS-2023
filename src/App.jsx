9b20c026096b6e1ca4a3ee6c4974c123b2cdebb8
azare242 <azare242@gmail.com>
2023-10-11 13:58:39 +0330

    feat: improve navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,52 @@
 import "./App.css";
-import "./css/Nav.css"
+import "./css/Nav.css";
 import { useState } from "react";
 import Home from "./Components/Home";
 import { Routes, Route, Link } from "react-router-dom";
 import NavItem from "./utils/NavItem";
 import FooterNew from "./Components/Footer";
 import HeaderNew from "./Components/Header";
-import AAISS from './assets/AAISS.png'
+import AAISS from "./assets/AAISS.png";
 function App() {
-
-  const [sect, setSect] = useState("/")
+  const [sect, setSect] = useState("/");
 
   return (
     <>
-      
-
       <nav className="nav">
-        <img src={AAISS} className='aaiss' href='/'/>
-        {/* <Link to="/" className="nav-item">
-          Home
-        </Link>  */}
-        <NavItem path='/' text='Home' set={setSect} sect={sect}/> 
-        <NavItem path='/Speakers' set={setSect} sect={sect}/>
-        <NavItem path='/Workshops' set={setSect} sect={sect}/>
-        <NavItem path='/Committee' set={setSect} sect={sect}/>
-        <NavItem path='/Schedule' set={setSect} sect={sect}/>
-        <NavItem path='/Organizer' set={setSect} sect={sect}/>
-        <NavItem path='/Workshops-and-Presentationst' text="All Workshops and Presentations" set={setSect} sect={sect}/>
-        <NavItem path='/Staff' set={setSect} sect={sect}/>
+      <div className="logo-container">
+          <Link to="/" className="nav-item">
+            <img
+              src={AAISS}
+              alt="Logo"
+              className="aaiss"
+              onClick={() => setSect("/")}
+            />
+          </Link>
+        </div>
+        <div className="nav-item-container">
+          <NavItem path="/" text="Home" set={setSect} sect={sect} />
+          <NavItem path="/About" set={setSect} sect={sect} />
+          <NavItem path="/Speakers" set={setSect} sect={sect} />
+          <NavItem path="/Workshops" set={setSect} sect={sect} />
+          <NavItem path="/Committee" set={setSect} sect={sect} />
+          <NavItem path="/Schedule" set={setSect} sect={sect} />
+          <NavItem path="/Organizer" set={setSect} sect={sect} />
+          <NavItem
+            path="/Workshops-and-Presentationst"
+            text="All Workshops and Presentations"
+            set={setSect}
+            sect={sect}
+          />
+          <NavItem path="/Staff" set={setSect} sect={sect} />
+        </div>
 
       </nav>
-      <HeaderNew/>
+      <HeaderNew />
 
       <Routes>
-        <Route path="/" 
-        element={
-          <Home/>
-        }/>
-
+        <Route path="/" element={<Home />} />
       </Routes>
-      <FooterNew/>
+      <FooterNew />
     </>
   );
 }
