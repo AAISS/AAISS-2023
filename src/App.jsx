510387b8f2ec4705927c2b796f7e074b31962b82
azare242 <azare242@gmail.com>
2023-10-11 13:40:37 +0330

    feat: add navbar and icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,45 @@
 import "./App.css";
-
+import "./css/Nav.css"
+import { useState } from "react";
+import Home from "./Components/Home";
+import { Routes, Route, Link } from "react-router-dom";
+import NavItem from "./utils/NavItem";
 import FooterNew from "./Components/Footer";
 import HeaderNew from "./Components/Header";
-
+import AAISS from './assets/AAISS.png'
 function App() {
+
+  const [sect, setSect] = useState("/")
+
   return (
     <>
-      <HeaderNew />
+      
 
-      <div className="main">
-        <h1 className="cms">Coming Soon</h1>
-      </div>
+      <nav className="nav">
+        <img src={AAISS} className='aaiss' href='/'/>
+        {/* <Link to="/" className="nav-item">
+          Home
+        </Link>  */}
+        <NavItem path='/' text='Home' set={setSect} sect={sect}/> 
+        <NavItem path='/Speakers' set={setSect} sect={sect}/>
+        <NavItem path='/Workshops' set={setSect} sect={sect}/>
+        <NavItem path='/Committee' set={setSect} sect={sect}/>
+        <NavItem path='/Schedule' set={setSect} sect={sect}/>
+        <NavItem path='/Organizer' set={setSect} sect={sect}/>
+        <NavItem path='/Workshops-and-Presentationst' text="All Workshops and Presentations" set={setSect} sect={sect}/>
+        <NavItem path='/Staff' set={setSect} sect={sect}/>
 
-      <FooterNew />
+      </nav>
+      <HeaderNew/>
+
+      <Routes>
+        <Route path="/" 
+        element={
+          <Home/>
+        }/>
+
+      </Routes>
+      <FooterNew/>
     </>
   );
 }
---
+++ src/Components/Home.jsx
@@ -0,0 +1,16 @@
+
+const Home = () => {
+  return (
+    <>
+
+      <div className="main">
+        <h1 className="cms">Coming Soon</h1>
+      </div>
+
+      
+    </>
+  );
+};
+
+
+export default Home;
