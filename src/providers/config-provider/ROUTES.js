bbfe0d9a9f343b4326f9152908dd764a67b1d135
Alireza <alireza.yousefpourm@gmail.com>
2023-10-13 15:04:39 +0330

    Added routes

---
+++ src/providers/config-provider/ROUTES.js
@@ -0,0 +1,40 @@
+const ROUTES = {
+    home: {
+        path: "/",
+        title: "Home",
+    },
+    about: {
+        path: "/about",
+        title: "About",
+    },
+    speakers: {
+        path: "/speakers",
+        title: "Speakers",
+    },
+    workshops: {
+        path: "/workshops",
+        title: "Workshops",
+    },
+    committee: {
+        path: "/committee",
+        title: "Committee",
+    },
+    schedule: {
+        path: "/schedule",
+        title: "Schedule",
+    },
+    organizer: {
+        path: "/organizer",
+        title: "Organizer",
+    },
+    wnp: {
+        path: "/wnp",
+        title: "Workshops and Presentations",
+    },
+    staff: {
+        path: "/staff",
+        title: "Staff",
+    },
+}
+
+export default ROUTES
