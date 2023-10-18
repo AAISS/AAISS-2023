d99df57af706e3748a251c6560d178b8fa26c1e5
Alireza <alireza.yousefpourm@gmail.com>
2023-10-18 22:25:53 +0330

    Added some useful functions to Helper

---
+++ src/utils/Helper.js
@@ -0,0 +1,48 @@
+function convertStringToDateWithoutTimezone(dateInString) {
+    const date = new Date(dateInString)
+    const computedDate = 60000 * date.getTimezoneOffset()
+    return new Date(date.getTime() + computedDate)
+}
+
+function convertDateTimeToDate(date) {
+    const weekend = [
+        'Sun',
+        'Mon',
+        'Tue',
+        'Wed',
+        'Thu',
+        'Fri',
+        'Sat'
+    ]
+    const months = [
+        'Jan',
+        'Feb',
+        'Mar',
+        'Apr',
+        'May',
+        'June',
+        'July',
+        'Aug',
+        'Sept',
+        'Oct',
+        'Nov',
+        'Dec',
+    ]
+    return weekend[date.getDay()]
+        + " "
+        + months[date.getMonth()]
+        + " "
+        + date.getDate()
+        + " "
+        + date.getFullYear()
+}
+
+function convertDateTimeToTime(date) {
+    return `${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}`
+}
+
+export const Helper = {
+    convertStringToDateWithoutTimezone,
+    convertDateTimeToDate,
+    convertDateTimeToTime,
+}
