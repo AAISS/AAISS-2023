263be7d50a5216d522dc93ce4952f49f226f2557
KimiaMontazeri <kimia.montazerii@snapp.cab>
2023-11-11 21:01:16 +0330

    feat: implement responsive AppBar component based on MUI

---
+++ src/Components/app-bar/AppBar.jsx
@@ -0,0 +1,117 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import Image from "../image/Image.jsx";
+import AAISS from "../../assets/AAISS.png";
+import { useConfig } from "../../providers/config-provider/ConfigProvider.jsx";
+import useNavItem from "../nav/nav-item/useNavItem.js";
+import { Link } from "react-router-dom";
+
+const drawerWidth = 240;
+
+const NavBarImage = () => (
+  <Image
+    src={AAISS}
+    alt={"aaiss logo"}
+    style={{
+      width: "10%",
+      height: "inherit",
+      padding: 10,
+    }}
+  />
+);
+
+export default function DrawerAppBar() {
+  const { ROUTES, currentRoute, setCurrentRoute } = useConfig();
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const { getVariant } = useNavItem();
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+      <NavBarImage />
+      <Divider />
+      <List>
+        {Object.keys(ROUTES).map((name, index) => (
+          <ListItem key={index} disablePadding>
+            <ListItemButton sx={{ textAlign: "center" }}>
+              <ListItemText primary={name} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <Box sx={{ display: "flex" }} className="nav">
+      <CssBaseline />
+      <AppBar component="nav" className="backdrop-color">
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: "none" } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <NavBarImage />
+          <Box sx={{ display: { xs: "none", sm: "block" } }}>
+            {Object.keys(ROUTES).map((name, index) => (
+              <Link to={ROUTES[name].path}>
+                <Button
+                  key={index}
+                  variant={getVariant(ROUTES[name].path, currentRoute.path)}
+                  onClick={() => setCurrentRoute(ROUTES[name])}
+                  sx={{
+                    color: "#fff",
+                  }}
+                >
+                  {name}
+                </Button>
+              </Link>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <nav className="nav">
+        <Drawer
+          //   container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </nav>
+    </Box>
+  );
+}
--- src/pages/MainContent.jsx
+++ src/pages/MainContent.jsx
@@ -1,34 +1,38 @@
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import FooterNew from "../Components/Footer.jsx";
 import Navbar from "../Components/nav/Navbar.jsx";
 import MobileNavbar from "../Components/nav/MobileNavBar.jsx";
-import {useConfig} from "../providers/config-provider/ConfigProvider.jsx";
+import { useConfig } from "../providers/config-provider/ConfigProvider.jsx";
 import ForgotPassword from "./ForgotPassword/ForgotPassword.jsx";
+import DrawerAppBar from "../Components/app-bar/AppBar.jsx";
 
 export default function MainContent() {
-    const {
-        ROUTES
-    } = useConfig()
+  const { ROUTES } = useConfig();
 
-    return (
-        <div className="main">
-            <Navbar/>
-            <MobileNavbar/>
-            <Routes>
-                {Object.keys(ROUTES).map(name => {
-                    return (
-                        <Route
-                            path={ROUTES[name].path}
-                            element={ROUTES[name]?.component}
-                            key={name}
-                        />
-                    )
-                })}
-            <Route path="/forgotpassword" element={<ForgotPassword/>} key="forgot"/>
-            </Routes>
-            <footer>
-                <FooterNew/>
-            </footer>
-        </div>
-    )
+  return (
+    <div className="main">
+      {/* <Navbar />
+      <MobileNavbar /> */}
+      <DrawerAppBar />
+      <Routes>
+        {Object.keys(ROUTES).map((name) => {
+          return (
+            <Route
+              path={ROUTES[name].path}
+              element={ROUTES[name]?.component}
+              key={name}
+            />
+          );
+        })}
+        <Route
+          path="/forgotpassword"
+          element={<ForgotPassword />}
+          key="forgot"
+        />
+      </Routes>
+      <footer>
+        <FooterNew />
+      </footer>
+    </div>
+  );
 }
