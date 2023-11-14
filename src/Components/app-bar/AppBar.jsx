d826f0045b11ceae6af001395b2f69eb8bad00ec
mh-zeynal <mh.zeynal@gmail.com>
2023-11-14 20:52:55 +0330

    chore: set home page link on logo located in appBar

--- src/Components/app-bar/AppBar.jsx
+++ src/Components/app-bar/AppBar.jsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Divider from "@mui/material/Divider";
@@ -14,7 +14,7 @@ import Toolbar from "@mui/material/Toolbar";
 import Button from "@mui/material/Button";
 import Image from "../image/Image.jsx";
 import AAISS from "../../assets/AAISS.png";
-import { useConfig } from "../../providers/config-provider/ConfigProvider.jsx";
+import {useConfig} from "../../providers/config-provider/ConfigProvider.jsx";
 import useNavItem from "../nav/nav-item/useNavItem.js";
 
 const drawerWidth = 240;
@@ -32,88 +32,112 @@ const NavBarImage = () => (
 );
 
 export default function DrawerAppBar() {
-  const { ROUTES, currentRoute, setCurrentRoute } = useConfig();
-  const [mobileOpen, setMobileOpen] = React.useState(false);
+    const {ROUTES, currentRoute, setCurrentRoute} = useConfig();
+    const [mobileOpen, setMobileOpen] = React.useState(false);
 
-  const { getVariant } = useNavItem();
+    const {getVariant} = useNavItem();
 
-  const handleDrawerToggle = () => {
-    setMobileOpen((prevState) => !prevState);
-  };
+    const handleDrawerToggle = () => {
+        setMobileOpen((prevState) => !prevState);
+    };
 
-  const drawer = (
-    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
-      <NavBarImage />
-      <Divider style={{ backgroundColor: "var(--dark-text-color)" }} />
-      <List>
-        {Object.keys(ROUTES).map((name, index) => (
-          <Link
-            to={ROUTES[name].path}
-            style={{ color: "white", textDecoration: "none" }}
-          >
-            <ListItem key={index} disablePadding>
-              <ListItemButton sx={{ textAlign: "center" }}>
-                <ListItemText primary={name} />
-              </ListItemButton>
-            </ListItem>
-          </Link>
-        ))}
-      </List>
-    </Box>
-  );
+    const drawer = (
+        <Box onClick={handleDrawerToggle} sx={{textAlign: "center"}}>
+            <Link to={ROUTES.home.path}
+                  className="logo-item"
+                  onClick={() => setCurrentRoute(ROUTES.home)}>
+                <Image
+                    src={AAISS}
+                    alt={"aaiss logo"}
+                    style={{
+                        width: "100px",
+                        height: "inherit",
+                        paddingRight: 24,
+                    }}
+                />
+            </Link>
+            <Divider style={{backgroundColor: "var(--dark-text-color)"}}/>
+            <List>
+                {Object.keys(ROUTES).map((name, index) => (
+                    <Link
+                        to={ROUTES[name].path}
+                        style={{color: "white", textDecoration: "none"}}
+                    >
+                        <ListItem key={index} disablePadding>
+                            <ListItemButton sx={{textAlign: "center"}}>
+                                <ListItemText primary={name}/>
+                            </ListItemButton>
+                        </ListItem>
+                    </Link>
+                ))}
+            </List>
+        </Box>
+    );
 
-  return (
-    <Box sx={{ display: "flex" }} className="nav">
-      <AppBar component="nav" className="backdrop-color">
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: "none" } }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <NavBarImage />
-          <Box sx={{ display: { xs: "none", sm: "block" } }}>
-            {Object.keys(ROUTES).map((name, index) => (
-              <Link to={ROUTES[name].path}>
-                <Button
-                  key={index}
-                  variant={getVariant(ROUTES[name].path, currentRoute.path)}
-                  onClick={() => setCurrentRoute(ROUTES[name])}
-                  sx={{
-                    color: "#fff",
-                  }}
+    return (
+        <Box sx={{display: "flex"}} className="nav">
+            <AppBar component="nav" className="backdrop-color">
+                <Toolbar>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        edge="start"
+                        onClick={handleDrawerToggle}
+                        sx={{mr: 2, display: {sm: "none"}}}
+                    >
+                        <MenuIcon/>
+                    </IconButton>
+                    <Link to={ROUTES.home.path}
+                          className="logo-item"
+                          onClick={() => setCurrentRoute(ROUTES.home)}>
+                        <Image
+                            src={AAISS}
+                            alt={"aaiss logo"}
+                            style={{
+                                width: "100px",
+                                height: "inherit",
+                                paddingRight: 24,
+                            }}
+                        />
+                    </Link>
+                    <Box sx={{display: {xs: "none", sm: "block"}}}>
+                        {Object.keys(ROUTES).map((name, index) => (
+                            <Link to={ROUTES[name].path}>
+                                <Button
+                                    key={index}
+                                    variant={getVariant(ROUTES[name].path, currentRoute.path)}
+                                    onClick={() => setCurrentRoute(ROUTES[name])}
+                                    sx={{
+                                        color: "#fff",
+                                    }}
+                                >
+                                    {name}
+                                </Button>
+                            </Link>
+                        ))}
+                    </Box>
+                </Toolbar>
+            </AppBar>
+            <nav className="nav">
+                <Drawer
+                    variant="temporary"
+                    open={mobileOpen}
+                    onClose={handleDrawerToggle}
+                    ModalProps={{
+                        keepMounted: true, // Better open performance on mobile.
+                    }}
+                    sx={{
+                        display: {xs: "block", sm: "none"},
+                        "& .MuiDrawer-paper": {
+                            boxSizing: "border-box",
+                            width: drawerWidth,
+                            background: "var(--background-color-lighter-20)",
+                        },
+                    }}
                 >
-                  {name}
-                </Button>
-              </Link>
-            ))}
-          </Box>
-        </Toolbar>
-      </AppBar>
-      <nav className="nav">
-        <Drawer
-          variant="temporary"
-          open={mobileOpen}
-          onClose={handleDrawerToggle}
-          ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
-          }}
-          sx={{
-            display: { xs: "block", sm: "none" },
-            "& .MuiDrawer-paper": {
-              boxSizing: "border-box",
-              width: drawerWidth,
-              background: "var(--background-color-lighter-20)",
-            },
-          }}
-        >
-          {drawer}
-        </Drawer>
-      </nav>
-    </Box>
-  );
+                    {drawer}
+                </Drawer>
+            </nav>
+        </Box>
+    );
 }
