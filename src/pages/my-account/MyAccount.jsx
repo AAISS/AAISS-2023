f898fab5fadceabbd8c0a406ddccf3076f911584
KimiaMontazeri <kimia.montazerii@snapp.cab>
2023-11-17 18:00:59 +0330

    refactor: use item-card in my account page

--- src/pages/my-account/MyAccount.jsx
+++ src/pages/my-account/MyAccount.jsx
@@ -1,80 +1,99 @@
+import React, { useState } from 'react';
+import { Box, Button, Divider, Stack, Tab, Tabs, Typography } from '@mui/material';
+import ItemCard from '../../components/item-card/item-card.jsx';
 import useMyAccount from './useMyAccount.js';
-import {Button, Tab, Tabs} from '@mui/material';
-import ObjListTable from '../../components/table/ObjListTable.jsx';
 
-export default function MyAccount() {
-    const {
-        sidebarItems,
-        tabsClickHandler,
-        setCurrentlySelectedSidebarItem,
-        currentlySelectedSidebarItem,
-        talks,
-        workshops,
-        basketItems
-    } =
-        useMyAccount();
+const TAB_ITEMS = ['Workshops', 'Talks', 'Cart'];
 
-    return (
-        <div
-            style={{
-                flexWrap: 'nowrap',
-                width: '100%',
-            }}
+const MyAccount = () => {
+  const { talks, workshops, cart } = useMyAccount();
+  const [tabValue, setTabValue] = useState(TAB_ITEMS[0]);
+
+  const handleChangeTab = (event, newValue) => {
+    setTabValue(newValue);
+  };
+
+  const getList = () => {
+    switch (tabValue) {
+      case 'Workshops':
+        return <List type="Workshops" items={workshops} />;
+      case 'Talks':
+        return <List type="Talks" items={talks} />;
+      case 'Cart':
+        return <List type="Cart" items={cart} />;
+      default:
+        return null;
+    }
+  };
+
+  const List = ({ type, items }) => {
+    return items.map((item, index) => (
+      <ItemCard
+        key={index}
+        isWorkshop={type === 'Workshops'}
+        purchaseState={type === 'Cart' ? 1 : 2}
+        title={item.name}
+        description={item.desc}
+        level={item.level}
+        startDate={item.start_date}
+        endDate={item.end_date}
+        presenterName={item.presenters[0]}
+        cost={item.cost}
+        hasProject={item.hasProject}
+        prerequisites={item.prerequisites}
+        syllabus={item.syllabus}
+        capacity={item.capacity}
+        isFull={item.isFull}
+        addToCalendarLink={item.addToCalendarLink}
+        onClickAddToCart={() => {}}
+        onClickRemoveFromCart={() => {}}
+      />
+    ));
+  };
+
+  const calculateTotalCost = () => {
+    let total = 0;
+    cart.forEach(({ cost }) => {
+      console.log(cost);
+      total += cost;
+    });
+    return total;
+  };
+
+  return (
+    <Stack alignItems="center">
+      <Box sx={{ bgcolor: 'var(--background-color)', px: 4, pb: 8, borderRadius: '30px', width: '80%' }}>
+        <Box sx={{ width: '100%' }}>
+          <Tabs value={tabValue} onChange={handleChangeTab} centered>
+            {TAB_ITEMS.map((item, index) => (
+              <Tab key={index} value={item} label={item} sx={{ color: 'var(--light-text-color)' }} />
+            ))}
+          </Tabs>
+        </Box>
+        <Stack
+          flexDirection="row"
+          gap={4}
+          justifyContent="center"
+          alignItems="flex-start"
+          marginTop={4}
+          flexWrap="wrap"
         >
-            <Tabs
-                style={{
-                    flex: 1,
-                    backgroundColor: 'green',
-                }}
-                variant={"fullWidth"}
-                value={currentlySelectedSidebarItem}
-                onChange={tabsClickHandler}
-            >
-                {Object.keys(sidebarItems).map((key, index) => {
-                    const element = sidebarItems[key];
-                    return (
-                        <Tab
-                            key={index}
-                            onClick={() => setCurrentlySelectedSidebarItem(element)}
-                            label={element.label}
-                        />
-                    );
-                })}
-            </Tabs>
-            <div
-                style={{
-                    flex: 5,
-                    minHeight: '300px',
-                    backgroundColor: 'red',
-                }}
-            >
-                {currentlySelectedSidebarItem.id === 'basket' && (
-                    <div>
-                        <ObjListTable title={currentlySelectedSidebarItem.label} data={basketItems}/>
-                        <div
-                            style={{
-                                display: 'flex',
-                                flexWrap: 'nowrap',
-                                flexDirection: 'row',
-                                justifyContent: 'space-around',
-                            }}
-                        >
-                            <div>Total: {'5000'}</div>
-                            <Button>Buy</Button>
-                        </div>
-                    </div>
-                )}
-                {currentlySelectedSidebarItem.id === 'workshops' && (
-                    <div>
-                        <ObjListTable title={currentlySelectedSidebarItem.label} data={workshops}/>
-                    </div>
-                )}
-                {currentlySelectedSidebarItem.id === 'talks' && (
-                    <div>
-                        <ObjListTable title={currentlySelectedSidebarItem.label} data={talks}/>
-                    </div>
-                )}
-            </div>
-        </div>
-    );
-}
+          {getList()}
+        </Stack>
+        {tabValue === 'Cart' && (
+          <>
+            <Divider sx={{ my: 2 }} />
+            <Stack alignItems="center" gap={1}>
+              <Typography>Total: {calculateTotalCost()} T</Typography>
+              <Button variant="contained" sx={{ px: 4 }}>
+                Buy
+              </Button>
+            </Stack>
+          </>
+        )}
+      </Box>
+    </Stack>
+  );
+};
+
+export default MyAccount;
--- src/pages/my-account/useMyAccount.js
+++ src/pages/my-account/useMyAccount.js
@@ -1,168 +1,163 @@
-import {useCallback, useEffect, useState} from "react";
-
 export default function useMyAccount() {
+  const talks = [
+    {
+      id: 1,
+      name: 'Discriminative Feature Learning and Face Recognition',
+      desc: 'پیش‌نیازهای کارگاه: \r\nKeras, Tensorflow, Convnet',
+      year: 2021,
+      level: 'NOT_ASSIGNED',
+      start_date: '2021-08-27T06:00:00Z',
+      end_date: '2021-08-31T18:00:00Z',
+      presenters: [],
+      cost: 50000,
+      hasProject: true,
+      prerequisites: 'Basic knowledge of machine learning',
+      syllabus: 'GANs in different type of problems - Various types of GANs - Coding',
+      capacity: 30,
+      isFull: false,
+      addToCalendarLink: 'https://google.com',
+    },
+    {
+      id: 2,
+      name: 'A tutorial on evaluating generative models',
+      desc: 'A tutorial on evaluating generative models',
+      year: 2021,
+      level: 'Elementary',
+      start_date: '2021-08-27T06:00:00Z',
+      end_date: '2021-08-31T18:00:00Z',
+      presenters: [10],
+      cost: 50000,
+      hasProject: false,
+      prerequisites: 'Basic knowledge of machine learning',
+      syllabus: 'GANs in different type of problems - Various types of GANs - Coding',
+      capacity: 50,
+      isFull: false,
+      addToCalendarLink: 'https://google.com',
+    },
+    {
+      id: 3,
+      name: 'on the privacy Risks of algorithmic fairness',
+      desc: 'on the privacy Risks of algorithmic fairness',
+      year: 2021,
+      level: 'NOT_ASSIGNED',
+      start_date: '2021-08-27T06:00:00Z',
+      end_date: '2021-08-30T18:00:00Z',
+      presenters: [11],
+      cost: 12000,
+      hasProject: true,
+      prerequisites: 'Basic knowledge of machine learning',
+      syllabus: 'GANs in different type of problems - Various types of GANs - Coding',
+      capacity: 20,
+      isFull: true,
+      addToCalendarLink: 'https://google.com',
+    },
+  ];
 
-    const [basketItems, setBasketItems] = useState({})
-    const [workshops, setWorkshops] = useState({})
-    const [talks, setTalks] = useState({})
+  const workshops = [
+    {
+      id: 4,
+      name: 'Foundations of Data Augmentation',
+      desc: 'Foundations of Data Augmentation',
+      year: 2021,
+      level: 'NOT_ASSIGNED',
+      start_date: '2021-08-27T06:00:00Z',
+      end_date: '2021-08-30T18:00:00Z',
+      presenters: [13],
+      cost: 50000,
+      hasProject: true,
+      prerequisites: 'Basic knowledge of machine learning',
+      syllabus: 'GANs in different type of problems - Various types of GANs - Coding',
+      capacity: 50,
+      isFull: true,
+      addToCalendarLink: 'https://google.com',
+    },
+    {
+      id: 5,
+      name: 'Representation Learning Without Labels',
+      desc: 'Representation Learning Without Labels',
+      year: 2021,
+      level: 'NOT_ASSIGNED',
+      start_date: '2021-08-27T06:00:00Z',
+      end_date: '2021-08-30T18:00:00Z',
+      presenters: [16],
+      cost: 20000,
+      hasProject: true,
+      prerequisites: 'Basic knowledge of machine learning',
+      syllabus: 'GANs in different type of problems - Various types of GANs - Coding',
+      capacity: 20,
+      isFull: false,
+      addToCalendarLink: 'https://google.com',
+    },
+    {
+      id: 6,
+      name: '3D CNNs with Adaptive Temporal Feature Resolutions',
+      desc: '3D CNNs with Adaptive Temporal Feature Resolutions',
+      year: 2021,
+      level: 'NOT_ASSIGNED',
+      start_date: '2021-08-27T06:00:00Z',
+      end_date: '2021-08-30T18:00:00Z',
+      presenters: [14],
+      cost: 50000,
+      hasProject: true,
+      prerequisites: 'Basic knowledge of machine learning',
+      syllabus: 'GANs in different type of problems - Various types of GANs - Coding',
+      capacity: 50,
+      isFull: false,
+      addToCalendarLink: 'https://google.com',
+    },
+    {
+      id: 7,
+      name: 'Learning Representations on Graphs',
+      desc: 'Learning Representations on Graphs',
+      year: 2021,
+      level: 'NOT_ASSIGNED',
+      start_date: '2021-08-27T06:00:00Z',
+      end_date: '2021-08-30T18:00:00Z',
+      presenters: [17],
+      cost: 50000,
+      hasProject: true,
+      prerequisites: 'Basic knowledge of machine learning',
+      syllabus: 'GANs in different type of problems - Various types of GANs - Coding',
+      capacity: 50,
+      isFull: false,
+      addToCalendarLink: 'https://google.com',
+    },
+    {
+      id: 8,
+      name: 'An Introduction to Autonomous Vehicles',
+      desc: 'An Introduction to Autonomous Vehicles',
+      year: 2021,
+      level: 'NOT_ASSIGNED',
+      start_date: '2021-08-27T06:00:00Z',
+      end_date: '2021-08-30T18:00:00Z',
+      presenters: [18],
+      cost: 50000,
+      hasProject: true,
+      prerequisites: 'Basic knowledge of machine learning',
+      syllabus: 'GANs in different type of problems - Various types of GANs - Coding',
+      capacity: 50,
+      isFull: false,
+      addToCalendarLink: 'https://google.com',
+    },
+  ];
 
-    const getUserTalks = useCallback(() => {
-        setTalks([
-            {
-                "id": 1,
-                "name": "Discriminative Feature Learning and Face Recognition",
-                "desc": "پیش‌نیازهای کارگاه: \r\nKeras, Tensorflow, Convnet",
-                "year": 2021,
-                "level": "NOT_ASSIGNED",
-                "start_date": "2021-08-27T06:00:00Z",
-                "end_date": "2021-08-31T18:00:00Z",
-                "presenters": []
-            },
-            {
-                "id": 2,
-                "name": "A tutorial on evaluating generative models",
-                "desc": "A tutorial on evaluating generative models",
-                "year": 2021,
-                "level": "Elementary",
-                "start_date": "2021-08-27T06:00:00Z",
-                "end_date": "2021-08-31T18:00:00Z",
-                "presenters": [
-                    10
-                ]
-            },
-            {
-                "id": 3,
-                "name": "on the privacy Risks of algorithmic fairness",
-                "desc": "on the privacy Risks of algorithmic fairness",
-                "year": 2021,
-                "level": "NOT_ASSIGNED",
-                "start_date": "2021-08-27T06:00:00Z",
-                "end_date": "2021-08-30T18:00:00Z",
-                "presenters": [
-                    11
-                ]
-            },
-        ])
-    }, [])
+  const cart = [
+    {
+      id: 4,
+      name: 'Foundations of Data Augmentation',
+      desc: 'Foundations of Data Augmentation',
+      year: 2021,
+      level: 'NOT_ASSIGNED',
+      start_date: '2021-08-27T06:00:00Z',
+      end_date: '2021-08-30T18:00:00Z',
+      presenters: [13],
+      cost: 120000,
+    },
+  ];
 
-    const getUserWorkshops = useCallback(() => {
-        setWorkshops([
-            {
-                "id": 4,
-                "name": "Foundations of Data Augmentation",
-                "desc": "Foundations of Data Augmentation",
-                "year": 2021,
-                "level": "NOT_ASSIGNED",
-                "start_date": "2021-08-27T06:00:00Z",
-                "end_date": "2021-08-30T18:00:00Z",
-                "presenters": [
-                    13
-                ]
-            },
-            {
-                "id": 5,
-                "name": "Representation Learning Without Labels",
-                "desc": "Representation Learning Without Labels",
-                "year": 2021,
-                "level": "NOT_ASSIGNED",
-                "start_date": "2021-08-27T06:00:00Z",
-                "end_date": "2021-08-30T18:00:00Z",
-                "presenters": [
-                    16
-                ]
-            },
-            {
-                "id": 6,
-                "name": "3D CNNs with Adaptive Temporal Feature Resolutions",
-                "desc": "3D CNNs with Adaptive Temporal Feature Resolutions",
-                "year": 2021,
-                "level": "NOT_ASSIGNED",
-                "start_date": "2021-08-27T06:00:00Z",
-                "end_date": "2021-08-30T18:00:00Z",
-                "presenters": [
-                    14
-                ]
-            },
-            {
-                "id": 7,
-                "name": "Learning Representations on Graphs",
-                "desc": "Learning Representations on Graphs",
-                "year": 2021,
-                "level": "NOT_ASSIGNED",
-                "start_date": "2021-08-27T06:00:00Z",
-                "end_date": "2021-08-30T18:00:00Z",
-                "presenters": [
-                    17
-                ]
-            },
-            {
-                "id": 8,
-                "name": "An Introduction to Autonomous Vehicles",
-                "desc": "An Introduction to Autonomous Vehicles",
-                "year": 2021,
-                "level": "NOT_ASSIGNED",
-                "start_date": "2021-08-27T06:00:00Z",
-                "end_date": "2021-08-30T18:00:00Z",
-                "presenters": [
-                    18
-                ]
-            },
-        ])
-    }, [])
-
-    const basketClickHandler = useCallback(() => {
-        setBasketItems([
-            {
-                "id": 4,
-                "name": "Foundations of Data Augmentation",
-                "desc": "Foundations of Data Augmentation",
-                "year": 2021,
-                "level": "NOT_ASSIGNED",
-                "start_date": "2021-08-27T06:00:00Z",
-                "end_date": "2021-08-30T18:00:00Z",
-                "presenters": [
-                    13
-                ]
-            },
-        ])
-    }, [])
-
-    const sidebarItems = {
-        workshops: {
-            id: "workshops",
-            label: "Workshops",
-            command: getUserWorkshops,
-        },
-        talks: {
-            id: "talks",
-            label: "Talks",
-            command: getUserTalks,
-        },
-        basket: {
-            id: "basket",
-            label: "Basket",
-            command: basketClickHandler,
-        },
-    }
-
-    const [currentlySelectedSidebarItem, setCurrentlySelectedSidebarItem] = useState(sidebarItems["workshops"])
-
-    const tabsClickHandler = useCallback((event, newValue) => {
-        console.log(newValue)
-        setCurrentlySelectedSidebarItem(newValue)
-    }, [setCurrentlySelectedSidebarItem])
-
-    useEffect(() => {
-        currentlySelectedSidebarItem.command()
-    }, [currentlySelectedSidebarItem])
-
-    return {
-        sidebarItems,
-        setCurrentlySelectedSidebarItem,
-        currentlySelectedSidebarItem,
-        talks,
-        workshops,
-        tabsClickHandler,
-        basketItems,
-    }
+  return {
+    talks,
+    workshops,
+    cart,
+  };
 }
