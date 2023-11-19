1309bdff5440bf621e8745da2eb9dd4b628c1605
Alireza <alireza.yousefpourm@gmail.com>
2023-11-20 01:35:53 +0330

    feat: displayed the parsed data

--- src/pages/workshops/WorkshopsPage.jsx
+++ src/pages/workshops/WorkshopsPage.jsx
@@ -1,14 +1,31 @@
+import ItemCard from "../../Components/item-card/item-card.jsx";
+import {Helper} from "../../utils/Helper.js";
 import useWorkshopsPage from "./useWorkshopsPage.js";
 
 export default function WorkshopsPage() {
 
     const {
-        parsedWorkshopsData
+        parsedItemsList
     } = useWorkshopsPage()
 
     return (
-        <pre>
-            {JSON.stringify(parsedWorkshopsData, null, 4)}
-        </pre>
+        <>
+            {parsedItemsList && parsedItemsList.map((e, index) => {
+
+                return (
+                    <ItemCard
+                        key={index}
+                        isWorkshop={e.isWorkshop}
+                        title={e.name}
+                        description={e.desc && Helper.omitLongString(e.desc, 100)}
+                        purchaseState={0}
+                        presenterName={e.presenters.join(", ")}
+                        startDate={e.start_date}
+                        endDate={e.end_date}
+                        level={e.level}
+                    />
+                )
+            })}
+        </>
     )
 }
