82b6435ee6ea341c57c906baba7716c6dd6a5230
Alireza <alireza.yousefpourm@gmail.com>
2023-11-20 01:35:10 +0330

    feat: fetch presentations data as well as workshops

--- src/pages/workshops/useWorkshopsPage.js
+++ src/pages/workshops/useWorkshopsPage.js
@@ -5,22 +5,41 @@ export default function useWorkshopsPage() {
     const {
         getWorkshopsData,
         workshopsData,
+        getPresentationsData,
+        presentationsData,
     } = useAPI()
 
-    const [parsedWorkshopsData, setParsedWorkshopsData] = useState()
+    const [parsedItemsList, setParsedItemsList] = useState()
 
     useEffect(() => {
         getWorkshopsData()
-    }, [getWorkshopsData])
+        getPresentationsData()
+    }, [getPresentationsData, getWorkshopsData])
 
     useEffect(() => {
-        if (workshopsData == null) return
+        if (workshopsData == null || presentationsData == null) return
 
+        const parsedData = workshopsData.concat(presentationsData).map(e => {
+            if ("is_full" in e) return null
+
+            const item = {}
+            item["id"] = e.id
+            item["name"] = e.name
+            item["start_date"] = e.start_date
+            item["end_date"] = e.end_date
+            item["level"] = e.level
+            console.log(e.presenters, e.teachers, e)
+            item.presenters = e.presenters ?? e.teachers
+            item["desc"] = e.desc
+            item["isWorkshop"] = "presenters" in e
+            return item
+        }).filter(e => e != null)
+        console.log(parsedData)
         //Parse workshopsData into parsedWorkshopsData
-        setParsedWorkshopsData(workshopsData)
-    }, [workshopsData])
+        setParsedItemsList(parsedData)
+    }, [workshopsData, presentationsData])
 
     return {
-        parsedWorkshopsData
+        parsedItemsList
     }
 }
