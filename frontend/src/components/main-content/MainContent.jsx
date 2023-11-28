aea3708c7cbc400657eeb326f7aae555abb47fcf
KimiaMontazeri <kimia.montazerii@snapp.cab>
2023-11-28 12:06:51 +0330

    feat: add route for PaymentCallbackPage

--- frontend/src/components/main-content/MainContent.jsx
+++ frontend/src/components/main-content/MainContent.jsx
@@ -1,5 +1,6 @@
 import { Route, Routes, useLocation } from 'react-router-dom';
 import ForgotPassword from '../../pages/ForgotPassword/ForgotPassword.jsx';
+import PaymentCallbackPage from '../../pages/payment-callback/PaymentCallbackPage.jsx';
 import { useConfig } from '../../providers/config-provider/ConfigProvider.jsx';
 import DrawerAppBar from '../app-bar/AppBar.jsx';
 import PageFooter from '../footer/PageFooter';
@@ -19,6 +20,7 @@ export default function MainContent() {
             return <Route path={ROUTES[name].path} element={ROUTES[name]?.component} key={name} />;
           })}
           <Route path="/forgotpassword" element={<ForgotPassword />} key="forgot" />
+          <Route path="/callback" element={<PaymentCallbackPage />} />
         </Routes>
       </main>
       {pathname !== '/' && <PageFooter />}
