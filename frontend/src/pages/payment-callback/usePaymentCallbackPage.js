8aa80dd5c1d6f31d47dc3429b557d3ac80ad1281
KimiaMontazeri <kimia.montazerii@snapp.cab>
2023-11-28 12:09:54 +0330

    fix: get clientrefid from query params and send it to payment verify endpoint

--- frontend/src/pages/payment-callback/usePaymentCallbackPage.js
+++ frontend/src/pages/payment-callback/usePaymentCallbackPage.js
@@ -1,59 +1,51 @@
-import {useConfig} from '../../providers/config-provider/ConfigProvider';
-import {useEffect, useState} from 'react';
-import {useAPI} from '../../providers/APIProvider/APIProvider';
+import { useEffect, useState } from 'react';
+import { useAPI } from '../../providers/APIProvider/APIProvider';
+import { useConfig } from '../../providers/config-provider/ConfigProvider';
 
 export default function usePaymentCallbackPage() {
-    const {
-        routeParams
-    } = useConfig()
+  const { routeParams } = useConfig();
 
-    const {
-        verifyPaymentData,
-        postVerifyPayment
-    } = useAPI()
+  const { verifyPaymentData, postVerifyPayment } = useAPI();
 
-    const [paymentStatus, setPaymentStatus] = useState(false)
-    const [paymentResultsData, setPaymentResultsData] = useState()
+  const [paymentStatus, setPaymentStatus] = useState(false);
+  const [paymentResultsData, setPaymentResultsData] = useState();
 
-    useEffect(() => {
-        if (routeParams == null)
-            return
+  useEffect(() => {
+    if (routeParams == null) return;
 
-        const refID = routeParams['clientrefid']
-        postVerifyPayment(0)
-    }, [routeParams])
+    const clientrefid = routeParams['clientrefid'];
+    postVerifyPayment({
+      clientrefid,
+    });
+  }, [routeParams]);
 
-    useEffect(() => {
-        if (verifyPaymentData == null)
-            return
+  useEffect(() => {
+    if (verifyPaymentData == null) return;
 
-        if (verifyPaymentData.status !== 200
-            || verifyPaymentData.data.status !== 200) {
-            setPaymentStatus(false)
-        } else {
-            setPaymentStatus(true)
-        }
-        setPaymentStatus(false)
-
-        const paymentResultTemp = {}
-        const keyDict = {
-            'message': "Message",
-            'refid': "Reference ID",
-            "card_number": "Credit Card Number"
-        }
-        Object.keys(verifyPaymentData.data).forEach(key => {
-            if (key in keyDict)
-                paymentResultTemp[keyDict[key]] = verifyPaymentData.data[key]
-            else
-                paymentResultTemp[key] = verifyPaymentData.data[key]
-        })
-        setPaymentResultsData(paymentResultTemp)
-        console.log(paymentResultTemp)
-    }, [verifyPaymentData])
-
-    return {
-        routeParams,
-        paymentStatus,
-        paymentResultsData,
+    if (verifyPaymentData.status !== 200 || verifyPaymentData.data.status !== 200) {
+      setPaymentStatus(false);
+    } else {
+      setPaymentStatus(true);
     }
+    setPaymentStatus(false);
+
+    const paymentResultTemp = {};
+    const keyDict = {
+      message: 'Message',
+      refid: 'Reference ID',
+      card_number: 'Credit Card Number',
+    };
+    Object.keys(verifyPaymentData.data).forEach((key) => {
+      if (key in keyDict) paymentResultTemp[keyDict[key]] = verifyPaymentData.data[key];
+      else paymentResultTemp[key] = verifyPaymentData.data[key];
+    });
+    setPaymentResultsData(paymentResultTemp);
+    console.log(paymentResultTemp);
+  }, [verifyPaymentData]);
+
+  return {
+    routeParams,
+    paymentStatus,
+    paymentResultsData,
+  };
 }
