f0396502cae246894108e3b7b1999865977fd849
Alireza Zare <azare242@gmail.com>
2023-11-29 01:46:01 +0330

    feat: url encode

--- backend/backend_api/models.py
+++ backend/backend_api/models.py
@@ -20,7 +20,7 @@ from backend_api import validators
 from backend_api.email import MailerThread
 from utils.random import create_random_string
 from utils.renderers import new_detailed_response
-
+from urllib.parse import quote
 SMALL_MAX_LENGTH = 255
 BIG_MAX_LENGTH = 65535
 
@@ -145,13 +145,13 @@ class Workshop(models.Model):
     @property
     def google_calendar_link(self):
         base_url = "https://www.google.com/calendar/render?action=TEMPLATE"
-        workshop_title = escape(self.name)
-        workshop_description = escape(self.desc)
+        workshop_title = quote(escape(self.name))
+        workshop_description = quote(escape(self.desc))
         start_datetime = self.start_date.strftime("%Y%m%dT%H%M%S")
         end_datetime = self.end_date.strftime("%Y%m%dT%H%M%S")
 
         teachers_names = " ".join([teacher.name for teacher in self.teachers.all()])
-        event_details = f"Teachers: {teachers_names}\nDescription: {workshop_description}"
+        event_details = quote(f"Teachers: {teachers_names}\nDescription: {workshop_description}")
 
         link = f"{base_url}&text={workshop_title}&dates={start_datetime}/{end_datetime}&details={event_details}"
 
@@ -215,13 +215,13 @@ class Presentation(models.Model):
     @property
     def google_calendar_link(self):
         base_url = "https://www.google.com/calendar/render?action=TEMPLATE"
-        presentation_title = escape(self.name)
-        presentation_description = escape(self.desc)
+        presentation_title = quote(escape(self.name))
+        presentation_description = quote(escape(self.desc))
         start_datetime = self.start_date.strftime("%Y%m%dT%H%M%S")
         end_datetime = self.end_date.strftime("%Y%m%dT%H%M%S")
 
         presenters_names = " ".join([presenter.name for presenter in self.presenters.all()])
-        event_details = f"Presenters: {presenters_names}\nDescription: {presentation_description}"
+        event_details = quote(f"Presenters: {presenters_names}\nDescription: {presentation_description}")
 
         link = f"{base_url}&text={presentation_title}&dates={start_datetime}/{end_datetime}&details={event_details}"
 
