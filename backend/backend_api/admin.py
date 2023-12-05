331d7d3d6328a025c0137044772f4d540d3aff37
Amirhesam Adibinia <hesamadibi80@gmail.com>
2023-12-06 02:37:03 +0330

    feat(backend): add sending login credentials email (#64)
    * feat(backend): add sending login credentials email
    
    * feat(backend): add retry policy on sending emails

--- backend/backend_api/admin.py
+++ backend/backend_api/admin.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib import admin
+from django.template.loader import render_to_string
 
+from aaiss_backend.settings import SKYROOM_URL
 from backend_api import models
 from backend_api.email import MailerThread
 from backend_api.models import Discount, Presentation, PresentationParticipation, WorkshopRegistration
@@ -37,52 +39,6 @@ class UserAdmin(admin.ModelAdmin):
     list_display = ('account',)
 
 
-class MailerForm(forms.ModelForm):
-    HTML_body = forms.CharField(widget=forms.Textarea)
-
-    class Meta:
-        model = models.Mailer
-        fields = '__all__'
-
-
-class MailerAdmin(admin.ModelAdmin):
-    model = models.Mailer
-    form = MailerForm
-
-    def execute_mailer(self, request, obj):
-        for mailer in obj:
-            if mailer.target_mode == 1:
-                targets = models.User.objects.all()
-                mails = []
-                for target in targets:
-                    mails.append(target.account.email)
-                MailerThread(mailer.subject, mails, mailer.HTML_body).start()
-            elif mailer.target_mode == 2:
-                mails = []
-                for workshop in mailer.workshop_selection.all():
-                    for user in models.User.objects.all():
-                        if workshop in user.registered_workshops.all():
-                            if __name__ == '__main__':
-                                mails.append(user.account.email)
-                MailerThread(mailer.subject, mails, mailer.HTML_body).start()
-            elif mailer.target_mode == 3:
-                mails = []
-                for user in models.User.objects.all():
-                    if user.registered_for_presentations:
-                        mails.append(user.account.email)
-                MailerThread(mailer.subject, mails, mailer.HTML_body).start()
-            elif mailer.target_mode == 4:
-                targets = mailer.user_selection.all()
-                mails = []
-                for target in targets:
-                    mails.append(target.account.email)
-                MailerThread(mailer.subject, mails, mailer.HTML_body).start()
-
-    execute_mailer.short_description = 'Send selected mails'
-    execute_mailer.allow_tags = True
-    actions = ['execute_mailer']
-
-
 class DiscountAdmin(admin.ModelAdmin):
     list_display = ('__str__', 'is_active', 'discount_percent', 'capacity', 'remaining_capacity', 'expiration_date')
     readonly_fields = ('participants',)
@@ -113,7 +69,7 @@ class PaymentAdmin(admin.ModelAdmin):
 class PresentationAdmin(admin.ModelAdmin):
     list_display = ('__str__', 'level', 'no_of_participants', 'year')
     readonly_fields = ('participants',)
-    actions = ['export_login_credentials']
+    actions = ['export_login_credentials', 'send_registration_emails']
 
     class Meta:
         model = Presentation
@@ -123,18 +79,34 @@ class PresentationAdmin(admin.ModelAdmin):
     def export_login_credentials(self, request, obj):
         user_credentials: list[SkyroomCredentials] = []
         for presentation in obj:
-            for presentation_registration in presentation.presentationparticipation_set.filter(
+            for registration in presentation.presentationparticipation_set.filter(
                     status=PresentationParticipation.StatusChoices.PURCHASED):
                 user_credentials.append(
-                    SkyroomCredentials(presentation_registration.username, presentation_registration.password,
-                                       presentation_registration.user.account.email))
+                    SkyroomCredentials(registration.username, registration.password,
+                                       registration.user.account.email))
         return convert_credentials_to_csv_response(user_credentials)
+
+    @admin.action(description='Send registration emails')
+    def send_registration_emails(self, request, obj):
+        for presentation in obj:
+            for registration in presentation.presentationparticipation_set.filter(
+                    status=PresentationParticipation.StatusChoices.PURCHASED):
+                MailerThread(f"AAISS login credentials for {presentation.name}",
+                             [registration.user.account.email],
+                             render_to_string('login_credentials.html',
+                                              {
+                                                  'username': registration.username,
+                                                  'password': registration.password,
+                                                  'meeting_url': SKYROOM_URL,
+                                                  'meeting_type': 'presentation',
+                                                  'meeting_title': presentation.name,
+                                              })).start()
 
 
 class WorkshopAdmin(admin.ModelAdmin):
     list_display = ('__str__', 'capacity', 'cost', 'has_project', 'level', 'no_of_participants', 'year')
     readonly_fields = ('participants',)
-    actions = ['export_login_credentials']
+    actions = ['export_login_credentials', 'send_registration_emails']
 
     class Meta:
         model = models.Workshop
@@ -144,12 +116,28 @@ class WorkshopAdmin(admin.ModelAdmin):
     def export_login_credentials(self, request, obj):
         user_credentials: list[SkyroomCredentials] = []
         for workshop in obj:
-            for workshop_registration in workshop.workshopregistration_set.filter(
+            for registration in workshop.workshopregistration_set.filter(
                     status=WorkshopRegistration.StatusChoices.PURCHASED):
                 user_credentials.append(
-                    SkyroomCredentials(workshop_registration.username, workshop_registration.password,
-                                       workshop_registration.user.account.email))
+                    SkyroomCredentials(registration.username, registration.password,
+                                       registration.user.account.email))
         return convert_credentials_to_csv_response(user_credentials)
+
+    @admin.action(description='Send registration emails')
+    def send_registration_emails(self, request, obj):
+        for workshop in obj:
+            for registration in workshop.workshopregistration_set.filter(
+                    status=WorkshopRegistration.StatusChoices.PURCHASED):
+                MailerThread(f"AAISS login credentials for {workshop.name}",
+                             [registration.user.account.email],
+                             render_to_string('login_credentials.html',
+                                              {
+                                                  'username': registration.username,
+                                                  'password': registration.password,
+                                                  'meeting_url': SKYROOM_URL,
+                                                  'meeting_type': 'workshop',
+                                                  'meeting_title': workshop.name,
+                                              })).start()
 
 
 class MiscAdmin(admin.ModelAdmin):
@@ -163,7 +151,6 @@ class MiscAdmin(admin.ModelAdmin):
 admin.site.register(models.Teacher, TeacherAdmin)
 admin.site.register(models.Presenter, PresenterAdmin)
 admin.site.register(models.User, UserAdmin)
-admin.site.register(models.Mailer, MailerAdmin)
 admin.site.register(models.Discount, DiscountAdmin)
 admin.site.register(models.Payment, PaymentAdmin)
 admin.site.register(models.WorkshopRegistration)
