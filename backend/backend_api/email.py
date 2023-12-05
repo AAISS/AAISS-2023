917ff9ef0ad8c7bdfeb5606d1e8aee36139ddbdb
Adibov <hesamadibi80@gmail.com>
2023-12-06 02:36:06 +0330

    feat(backend): add retry policy on sending emails

--- backend/backend_api/email.py
+++ backend/backend_api/email.py
@@ -1,4 +1,5 @@
 import threading
+from time import sleep
 
 from django.conf import settings
 from django.core.mail import EmailMessage
@@ -8,6 +9,9 @@ from aaiss_backend.settings import ENABLE_SENDING_EMAIL
 
 
 class MailerThread(threading.Thread):
+    _MAXIMUM_RETRIES = 5
+    _SLEEP_INTERVAL_SECONDS = 5
+
     def __init__(self, subject: str, targets: list[str], html_body: str):
         self.subject = subject
         self.targets = targets
@@ -28,7 +32,17 @@ class MailerThread(threading.Thread):
         )
         email.content_subtype = "html"
         if ENABLE_SENDING_EMAIL:
-            email.send(fail_silently=False)
+            has_send = False
+            for i in range(self._MAXIMUM_RETRIES):
+                try:
+                    email.send(fail_silently=True)
+                    has_send = True
+                    break
+                except Exception as e:
+                    print(f"Exception raised while sending email: {e}")
+                    sleep(self._SLEEP_INTERVAL_SECONDS)
+            if not has_send:
+                print(f"Failed to send email to {self.targets} after {self._MAXIMUM_RETRIES} retries")
         else:
             print(f"Sent the following email (change ENABLE_SENDING_EMAIL to True to actually send the email):")
             print(f"To: {email.bcc}\nSubject: {email.subject}\n\nBody: {email.body}\n\n")
