dfa9c9e7385ebba357bac4702083508e46106133
Alireza Zare <78508222+azare242@users.noreply.github.com>
2023-12-06 01:07:10 +0330

    feat(backend): add export Skyroom credentials action (#45)
    * feat: extract emails for every w/p
    
    * add json files to gitignore
    
    * feat: xlsx
    
    * feat(backend): make exporter an action & change output format to .csv
    
    ---------
    
    Co-authored-by: Adibov <hesamadibi80@gmail.com>

---
+++ backend/utils/skyroom_exporter.py
@@ -0,0 +1,21 @@
+import csv
+from dataclasses import dataclass
+
+from django.http import HttpResponse
+
+
+@dataclass
+class SkyroomCredentials:
+    username: str
+    password: str
+    full_name: str
+
+
+def convert_credentials_to_csv_response(credentials: list[SkyroomCredentials]) -> HttpResponse:
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="login_credentials.csv"'
+    writer = csv.writer(response)
+    writer.writerow(['username', 'password', 'full_name', 'room', 'access'])
+    for credential in credentials:
+        writer.writerow([credential.username, credential.password, credential.full_name, 'aaiss', 'normal'])
+    return response
