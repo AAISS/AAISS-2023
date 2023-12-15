b2db25d52d509f61b8a039889a07eea3b92bbbb9
Alireza <alireza.yousefpourm@gmail.com>
2023-12-16 01:02:30 +0330

    fix: rollback unwanted changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,7 @@
 import react from '@vitejs/plugin-react';
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        middlewares: [
-            (req, res, next) => {
-                // Force the method to be GET
-                req.method = 'GET';
-
-                // Your logic to generate and send the HTML page
-                next();
-            },
-        ],
-    }
+  plugins: [react()],
 });
