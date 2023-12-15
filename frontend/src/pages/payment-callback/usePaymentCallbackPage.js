d0ec7328bc595d8fbfc2aac6da7864263bf57057
Alireza <alireza.yousefpourm@gmail.com>
2023-12-16 01:01:07 +0330

    feat: removed verify API and changed callback page flow

--- frontend/src/pages/payment-callback/usePaymentCallbackPage.js
+++ frontend/src/pages/payment-callback/usePaymentCallbackPage.js
@@ -1,60 +1,55 @@
-import { useEffect, useState } from 'react';
-import { useAPI } from '../../providers/APIProvider/APIProvider';
-import { useConfig } from '../../providers/config-provider/ConfigProvider';
+import {useEffect, useState} from 'react';
+import {useConfig} from '../../providers/config-provider/ConfigProvider';
 
 export default function usePaymentCallbackPage() {
-  const { routeParams } = useConfig();
+    const {routeParams} = useConfig();
 
-  const { verifyPaymentData, postVerifyPayment } = useAPI();
+    const [paymentStatus, setPaymentStatus] = useState(false);
+    const [paymentResultsData, setPaymentResultsData] = useState();
+    const [isLoading, setIsLoading] = useState(true)
 
-  const [paymentStatus, setPaymentStatus] = useState(false);
-  const [paymentResultsData, setPaymentResultsData] = useState();
-  const [isLoading, setIsLoading] = useState(true)
+    useEffect(() => {
+        if (routeParams == null) return;
 
-  useEffect(() => {
-    if (routeParams == null) return;
+        const refID = routeParams['client_ref_id'];
+        const status = routeParams['payment_status']
+        if (!refID && !status)
+            return
 
-    const clientrefid = routeParams['clientrefid'];
-    if (!clientrefid)
-      return
-    postVerifyPayment({
-      clientrefid,
-    });
-  }, [postVerifyPayment, routeParams]);
+        const keysDict = {
+            status: "Status",
+            redID: "Reference ID"
+        }
+        const paymentResultTemp = {}
 
-  useEffect(() => {
-    if (verifyPaymentData == null) return;
+        if (status) {
+            const statusValues = {
+                succeeded: true,
+                failed: false,
+            }
 
-    const paymentResultTemp = {};
-    if (verifyPaymentData.status !== 200 || verifyPaymentData.data.status !== 200) {
-      setPaymentStatus(false);
-      paymentResultTemp['Message'] = "Payment Failed!"
-    } else {
-      paymentResultTemp['Message'] = "Success!"
-      setPaymentStatus(true);
-    }
+            const statusDict = {
+                true: "Success!",
+                false: "Failure!"
+            }
 
-    const keyDict = {
-      message: 'Message',
-      refid: 'Reference ID',
-      card_number: 'Credit Card Number',
-      data: "Reference ID"
+            const statusBool = statusValues[status] ?? false
+            setPaymentStatus(statusBool)
+            paymentResultTemp[keysDict.status] = statusDict[statusBool]
+        }
+
+        if (refID) {
+            paymentResultTemp[keysDict.redID] = refID
+        }
+
+        setPaymentResultsData(paymentResultTemp);
+        setIsLoading(false)
+    }, [routeParams]);
+
+    return {
+        routeParams,
+        paymentStatus,
+        isLoading,
+        paymentResultsData,
     };
-    const removedKeys = ['status', 'message']
-    Object.keys(verifyPaymentData.data).forEach((key) => {
-      if (removedKeys.indexOf(key) > -1)
-        return
-      if (key in keyDict) paymentResultTemp[keyDict[key]] = verifyPaymentData.data[key];
-      else paymentResultTemp[key] = verifyPaymentData.data[key];
-    });
-    setPaymentResultsData(paymentResultTemp);
-    setIsLoading(false)
-  }, [verifyPaymentData]);
-
-  return {
-    routeParams,
-    paymentStatus,
-    isLoading,
-    paymentResultsData,
-  };
 }
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,18 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    server: {
+        middlewares: [
+            (req, res, next) => {
+                // Force the method to be GET
+                req.method = 'GET';
+
+                // Your logic to generate and send the HTML page
+                next();
+            },
+        ],
+    }
 });
