5d6d7d7259ad855cb247ca906238710b7c932d47
amirhosseinaghighi <amirhossein.aghighi2@gmail.com>
2025-01-10 19:34:10 +0330

    Fixes Gmail issue for the email field on signup and sets up Tailwind

--- frontend/src/utils/Email.js
+++ frontend/src/utils/Email.js
@@ -8,7 +8,7 @@ export const hasEmailError = (email) => {
   const result = trimmedEmail
     .toLowerCase()
     .match(
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      /^[a-zA-Z0-9._%+-]+@gmail\.com$/,
     );
 
   return result === null ? true : false;
